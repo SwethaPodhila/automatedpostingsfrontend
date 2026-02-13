bce8d65b4bad9236f6ca3bfbba451dc1f7f11c9f
SwethaPodhila <venkannapodhila@gmail.com>
2026-02-13 18:23:11 +0530

    link

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -55,7 +55,7 @@ export default function Login() {
         setError("");
 
         try {
-            const res = await axios.post("http://localhost:5000/user/login", form);
+            const res = await axios.post("https://api.aiwingsglobal.com/user/login", form);
             if (res.data.success) {
                 localStorage.setItem("token", res.data.token);
                 localStorage.setItem("userId", res.data.userId);
